e5663c714a28f4fd4170a4e092a6825b9462887a
Lucas1774 <lucas.115@hotmail.com>
2023-11-01 19:41:22 +0100

    Complete a first draft TODO: handle locks TODO: make it work

--- 1/Trabajo1/Demonio.sh
+++ 1/Trabajo1/Demonio.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
-# Dummy Demon, you have to complete it to make it do something
+# Function to revive a process and log it in the Bible
+revive_process() {
+  local pid="$1"
+  local command="$2"
 
-# Loop until the apocalypse arrives
-#   - Wait for one second
-#   - Read the lists and revive processes when necessary, leaving entries in the Bible
-#   - You can use as many temporary files as you want, but they must be deleted in the Apocalypse
-#   - Use a lock to avoid accessing the lists at the same time as Fausto
-#   - Be careful when closing processes; you must terminate the entire tree, not just one of them
-# End of the loop
+  # If it is not running, run it
+  if ! kill -0 "$pid" 2>/dev/null; then
+    bash -c "$command" &
 
-# Apocalypse: terminate all processes and clean up everything, leaving only Fausto, the Demon, and the Bible
+    # Log the revival in Biblia.txt
+    echo "$(date '+%T') El proceso $pid '$command' ha resucitado." >>Biblia.txt
+  fi
+}
 
-# Trap to handle cleanup during the Apocalypse
-trap 'kill $(jobs -p)' EXIT
+# Function to process a list of processes
+process_processes() {
+  local list_file="$1"
 
-# Loop until the apocalypse arrives
-while true; do
-  # Wait for one second
+  # Check and maintain the list
+  if [ -f "$list_file" ]; then
+    while IFS= read -r line; do
+      pid=$(echo "$line" | awk '{print $1}')
+      command_to_run=$(echo "$line" | cut -d' ' -f2-)
+
+      if [ -e "Infierno/$pid" ]; then
+        # Kill the entire process tree
+        pkill -P "$pid"
+        rm -f "Infierno/$pid"
+        echo "$(date '+%T') El proceso $pid '$command_to_run' ha sido destruido." >>Biblia.txt
+      elif ! kill -0 "$pid" 2>/dev/null; then
+        # TODO: Remove the entry if the process is no longer running
+        echo "$(date '+%T') El proceso $pid '$command_to_run' ha terminado." >>Biblia.txt
+      fi
+    done <"$list_file"
+  fi
+}
+
+# Loop until Apocalipsis arrives
+while ! [ -f "Apocalipsis" ]; do
   sleep 1
-
-  # Acquire a lock to access the lists
-  exec 9<>.lock
-
-  # Read the "procesos" file to check for processes to revive
-  while read -r line; do
-    pid=$(echo "$line" | awk '{print $1}')
-    command_to_run=$(echo "$line" | cut -d' ' -f2-)
-
-    # Check if the process is still alive
-    if ! kill -0 "$pid" 2>/dev/null; then
-      # Revive the process in the background
-      bash -c "$command_to_run" &
-    fi
-  done < procesos
-
-  # Release the lock
-  exec 9>&-
+  process_processes "procesos"
+  process_processes "procesos_servicio"
+  process_processes "procesos_periodicos"
 done
+
+echo "$(date '+%T') Se acabó el mundo." >>Biblia.txt
--- 1/Trabajo1/Fausto.sh
+++ 1/Trabajo1/Fausto.sh
@@ -1,54 +1,124 @@
 #!/bin/bash
 
-# Receives commands, creating the appropriate processes and lists
-# If the Demon is not alive, it creates it
-# When reading/writing to the lists, locking must be used to avoid conflicts with the Demon
-
+# -----lAUNCHING "DEMONIO"-----
 # Check if the "Demonio" process is running
-if ! pgrep -x "Demonio" > /dev/null; then
-    # If the "Demonio" process is not running, start it and perform setup tasks
-    ./Demonio > /dev/null 2>&1 &
-    
-    # Remove files and directory if they exist
-    rm -f procesos procesos_servicio procesos_periodicos Biblia.txt SanPedro Apocalipsis
-    rm -rf Infierno
+if ! pgrep -x "Demonio" >/dev/null; then
+  # Remove files and directory if they exist
+  rm -f procesos procesos_servicio procesos_periodicos Biblia.txt SanPedro Apocalipsis
+  rm -rf Infierno
 
-    # Create the necessary files and folders
-    touch procesos procesos_servicio procesos_periodicos Biblia.txt SanPedro
-    mkdir Infierno
+  # Create the necessary files and folders
+  touch procesos procesos_servicio procesos_periodicos Biblia.txt SanPedro
+  mkdir Infierno
+
+  # If the "Demonio" process is not running, start it and perform setup tasks
+  ./Demonio.sh >/dev/null 2>&1 &
+
+  # Record the creation of the demon in Biblia.txt
+  echo "$(date '+%T'): ---------------Génesis---------------" >>Biblia.txt
+  echo "$(date '+%T') El demonio ha sido creado" >>Biblia.txt
 fi
 
-# Use a case block to handle different valid arguments
-case "$1" in
-  run)
-    # Check if the 'run' command is provided as an argument
-    if [ -z "$2" ]; then
-      echo "Usage: $0 run <command>"
-      exit 1
-    fi
-
-    # Extract the command to be executed
-    command_to_run="${@:2}"
-
-    # Run the command in the background
-    bash -c "$command_to_run" &
-
-    # Get the PID of the Bash process that is executing the command
-    pid=$!
-
-    # Create an entry in the process list file
-    echo "$pid '$command_to_run'" >> procesos
-
-    # Record the process birth event in Biblia.txt
-    echo "$(date '+%T:%M:%S'): The process $pid '$command_to_run' has been born." >> Biblia.txt
-
-    # Exit immediately
-    exit 0
-    ;;
-
-  # Add more valid arguments here if needed
-  *)
-    echo "Usage: $0 {run}"
+# -----RUN COMMAND-----
+# Function to check if the required number of arguments are provided
+check_arguments() {
+  local command_name="$1"
+  local required_arg_count="$2"
+  local recieved_arg_count="$3"
+  if [ $recieved_arg_count -ne "$required_arg_count" ]; then
+    echo "Uso: $0 $command_name <arg1> <arg2> ..."
     exit 1
-    ;;
+  fi
+}
+
+# Function to record an event
+record_event() {
+  local pid_file="$1"
+  local command="$2"
+  local pid=$!
+  echo "$pid '$command'" >>"$pid_file"
+  echo "$(date '+%T'): El proceso $pid '$command' ha nacido." >>Biblia.txt
+}
+
+# Script
+case "$1" in
+run)
+  check_arguments "$1" 2 $#
+  command="${@:2}"
+  bash -c "$command" &
+  pid=$!
+
+  # Record the event list and bible
+  echo "$pid '$command'" >>"procesos"
+  echo "$(date '+%T'): El proceso $pid '$command' ha nacido." >>Biblia.txt
+  exit 0
+  ;;
+
+run-service)
+  check_arguments "$1" 2 $#
+  service_command="${@:2}"
+  bash -c "$service_command" &
+  service_pid=$!
+
+  # Record the event list and bible
+  echo "$service_pid '$service_command'" >>"procesos_servicio"
+  echo "$(date '+%T'): El proceso servicio $service_pid '$service_command' ha nacido." >>Biblia.txt
+  exit 0
+  ;;
+
+run-periodic)
+  check_arguments "$1" 3 $#
+  period="$2"
+  periodic_command="${@:3}"
+  bash -c "$periodic_command" &
+  pid=$!
+
+  # Record the event list and bible
+  echo "0 $period $pid '$periodic_command'" >>"procesos_periodicos"
+  echo "$(date '+%T'): El proceso periódico $pid '$periodic_command' ha nacido." >>Biblia.txt
+  exit 0
+  ;;
+
+list)
+  check_arguments "$1" 1 $#
+  echo "Contents of list files:"
+  cat "procesos"
+  cat "procesos_servicio"
+  cat "procesos_periodicos"
+  exit 0
+  ;;
+
+help)
+  check_arguments "$1" 1 $#
+  echo "Comandos disponibles y su sintaxis:"
+  echo "./Fausto.sh run <comando>"
+  echo "./Fausto.sh run-service <comando>"
+  echo "./Fausto.sh run-periodic <periodo> <comando>"
+  echo "./Fausto.sh list (Mostrar el contenido de las listas: procesos, procesos_servicio y procesos_periodicos)"
+  echo "./Fausto.sh help (Mostrar los comandos disponibles y su sintaxis)"
+  echo "./Fausto.sh stop <PID> (Detener un proceso por su PID)"
+  exit 0
+  ;;
+
+stop)
+  check_arguments "$1" 2 $#
+  local pid_to_stop="$2"
+
+  if [ -f "procesos" ] || [ -f "procesos_servicio" ] || [ -f "procesos_periodicos" ]; then
+    # Check if the PID exists in any of the lists
+    if grep -q "$pid_to_stop" procesos procesos_servicio procesos_periodicos; then
+      # Create a file in the 'Infierno' folder to signal the demon to stop the process
+      touch "Infierno/$pid_to_stop"
+    else
+      echo "PID $pid_to_stop no está en las listas. Usa './Fausto.sh list'"
+    fi
+  else
+    echo "Las listas (procesos, procesos_servicio, and procesos_periodicos) no existen o están vacías."
+  fi
+  ;;
+
+*)
+  echo "Uso: $0 {run | run-service | run-periodic | list | help | stop}"
+  exit 1
+  ;;
 esac
