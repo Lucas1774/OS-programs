f198bf90e008c26d1899526f4dc98f888c7acfe2
Lucas1774 <lucas.115@hotmail.com>
2023-11-19 17:02:57 +0100

    Ammend previous

--- 1/Trabajo1/Demonio.sh
+++ 1/Trabajo1/Demonio.sh
@@ -18,20 +18,6 @@ kill_tree() {
   kill -SIGTERM $pid
 }
 
-# Function to revive a process and log it in the Bible
-revive_process() {
-  local pid="$1"
-  local command="$2"
-
-  # If it is not running, run it
-  if ! kill -0 "$pid" 2>/dev/null; then
-    bash -c "$command" &
-
-    # Log the revival in Biblia.txt
-    echo "$(date '+%T') El proceso $pid '$command' ha resucitado." >>"$LOG_FILE"
-  fi
-}
-
 # Function to process a list of processes
 process() {
   local list_file="$1"
@@ -49,54 +35,33 @@ process() {
         rm -f "$HELL_DIR/$pid"
         echo "$(date '+%T') El proceso $pid '$command_to_run' ha sido destruido." >>"$LOG_FILE"
       # If dead
-      elif ! ps | grep "$pid"; then
+      elif ! ps "$pid" > /dev/null; then
         flock "$LOCK_FILE" sed -i "/$pid/d" "$list_file"
         # If process remove from list
-        if [ "$list_file" eq $PROCESS_LIST]; then
-          echo "$(date '+%T') El proceso $pid '$command_to_run' ha terminado." >>"$LOG_FILE"
+        if [ "$list_file" == "$PROCESS_LIST" ]; then
+          echo "$(date '+%T') Process $pid '$command_to_run' has terminated." >>"$LOG_FILE"
         # If service revive
-        elif [ "$list_file" eq $SERVICE_LIST]; then
+        elif [ "$list_file" == "$SERVICE_LIST" ]; then
           bash -c "$command_to_run" &
-          flock "$LOCK_FILE" echo $! '$command_to_run' >> $list_file
-          echo "$(date '+%T') El servicio $pid '$command_to_run' ha resucitado." >>"$LOG_FILE"
+          new_pid=$!
+          flock "$LOCK_FILE" echo "$new_pid '$command_to_run'" >> "$list_file"
+          echo "$(date '+%T') El servicio $pid $command_to_run ha resucitado." >>"$LOG_FILE"
+        fi
       fi
-    done
+    done < "$list_file"
   fi
 }
 
 # Function to process a list of periodic processes
-process_periodic() {
-  local list_file="$1"
-
-  # Check and maintain the list
-  if [ -f "$list_file" ]; then
-    # Iterate through lines
-    while IFS= read -r line; do
-      pid=$(echo "$line" | awk '{print $1}')
-      command_to_run=$(echo "$line" | cut -d' ' -f2-)
-      # If in hell folder -> kill tree, remove from queue
-      if [ -e "$HELL_DIR/$pid" ]; then
-        flock "$LOCK_FILE" sed -i "/$pid/d" "$list_file"
-        kill_tree "$pid"
-        rm -f "$HELL_DIR/$pid"
-        echo "$(date '+%T') El proceso $pid '$command_to_run' ha sido destruido." >>"$LOG_FILE"
-      # If dead -> remove from queue
-      elif ! ps | grep "$pid"; then
-        flock "$LOCK_FILE" sed -i "/$pid/d" "$list_file"
-        echo "$(date '+%T') El proceso $pid '$command_to_run' ha terminado." >>"$LOG_FILE"
-      fi
-    done
-  fi
-}
-
-
+# process_periodic() {
+# }
 
 # Loop until Apocalipsis arrives
 until [ -f "$APOCALIPSIS_FILE" ]; do
   sleep 1
   process "$PROCESS_LIST"
   process "$SERVICE_LIST"
-  process_periodic "$PERIODIC_LIST"
+  # process_periodic "$PERIODIC_LIST"
 done
 
 # Remove files and hell directory
