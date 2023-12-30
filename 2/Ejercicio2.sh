8a60aeb24bc39a6d7981c04b388360e16b2d1703
Lucas1774 <lucas.115@hotmail.com>
2023-12-30 03:50:36 +0100

    Final commit

--- 2/Ejercicio2.sh
+++ 2/Ejercicio2.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 #este archivo es un scrip que:
 #compila los fuentes padre.c e hijo.c con gcc
+cd ./Trabajo2
+gcc padre.c -o padre
+gcc hijo.c -o hijo
 #crea el fihero fifo "resultado"
-#lanza un cat en segundo plano para leer "resultado"  
+mkfifo resultado
+#lanza un cat en segundo plano para leer "resultado"
+cat resultado &
 #lanza el proceso padre
+./padre ./padre 10
 #al acabar limpia todos los ficheros que ha creado
 #borra los exe y el FIFO
+rm padre
+rm hijo
+rm resultado
